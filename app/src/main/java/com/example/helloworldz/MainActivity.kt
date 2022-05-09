3643f4588525c070d1bf34cc1640aeb7804027f4
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-09 06:35:00 +0300

    calculate bmi and send money

---
+++ app/src/main/java/com/example/helloworldz/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.helloworldz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btncalculate:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btncalculate=findViewById(R.id.btnBMI)
+        btncalculate.setOnClickListener{
+            val intent=Intent(this,BMICalculate::class.java)
+            startActivity(intent)
+        }
+
+          lateinit var btnSendMoney: Button
+          btnSendMoney=findViewById(R.id.btnSendMoney)
+          btnSendMoney.setOnClickListener {
+            val intent =Intent(this,SendMoney::class.java)
+              startActivity(intent)
+        }
+        }
+    }
